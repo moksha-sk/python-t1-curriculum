e87dbed0b3076eca3351c833c2c6a95cb49eb3f2
moksha-sk <moksha.k@outlook.com>
2025-11-12 17:54:32 -0800

    Update divisibility.py

---
+++ lesson3/classwork/divisibility.py
@@ -0,0 +1,5 @@
+num = int(input("Enter a number: ")) 
+if (5>3): # Only runs if comparison is true
+  print("f")
+  if num % 3: 
+    print("This number is divisible by 3!") # only runs if number is divisible by three
