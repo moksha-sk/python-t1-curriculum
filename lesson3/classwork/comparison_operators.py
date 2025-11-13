a7cb477e35ce62c45699b05d402e565ec7ae9d5e
moksha-sk <moksha.k@outlook.com>
2025-11-12 17:47:31 -0800

    Update comparison_operators.py

---
+++ lesson3/classwork/comparison_operators.py
@@ -0,0 +1,18 @@
+# The < or> symbol coms first
+# Two equals signs for comparison, one equal sign for assignment to variable
+print(5>3)   # greater than
+print (5<3)  #less than 
+a=10
+b=7 print(a>b) print(a==bb)
+
+x=int(input("Give me a number: "))
+y=int(input("Give me another number: "))
+print(x<y)
+
+c="apple"
+d="banana"
+print("apple"=="apple")
+print("apple=="Apple") # Case matters when comparing strings
+print(c!=d)
+print"cat"> "dog") # Alphabetical comparison when greater that and less than
+print("dog"<"zebra")
