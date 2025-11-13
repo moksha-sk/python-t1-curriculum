6cf23ced678dcebf51c0921cb0eb706be17c96a3
moksha-sk <moksha.k@outlook.com>
2025-11-12 18:02:32 -0800

    Update if_else.py

---
+++ lesson3/classwork/if_else.py
@@ -0,0 +1,15 @@
+day = input("What day of the week is it?")
+
+if day == "monday":
+  print("Ugh, it's Monday)  
+elif day == "friday":
+        print(Yay, it's almost the weekend!")
+ elif day=="saturday" :
+        print("It's the weekend")
+ else:
+     print"(It's just a regular weekday.")
+
+     score= int(input("your score out of 100: "))if score.= 60:
+     print("You passed!!") 
+     if score >=90: 
+     print(You" got an A!!!!!!!!!!!!!!")
